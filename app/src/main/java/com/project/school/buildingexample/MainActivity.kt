e0fc7dc174abf6c7be1bb4194a961208faec745c
Lukáš <zateklukas@gmail.com>
2018-02-13 22:57:59 +0100

    Added Cloud Added swipe gesture to rotate model Added tap gesture to toggle raining

--- app/src/main/java/com/project/school/buildingexample/MainActivity.kt
+++ app/src/main/java/com/project/school/buildingexample/MainActivity.kt
@@ -1,17 +1,50 @@
 package com.project.school.buildingexample
 
 import android.os.Bundle
+import android.support.v4.view.GestureDetectorCompat
+import android.view.GestureDetector
+import android.view.MotionEvent
 import eu.kudan.kudan.*
 import io.reactivex.Observable
 import java.util.concurrent.TimeUnit
-import com.jme3.math.Quaternion
-import java.util.*
 
 
 class MainActivity : ARActivity(), ARArbiTrackListener {
 
     private val intervalObservable = Observable.interval(6, TimeUnit.MILLISECONDS)
-    private val trackableId = "StarWars"
+    private val gestureDetector by lazy {
+        GestureDetectorCompat(this,object : GestureDetector.OnGestureListener {
+            override fun onShowPress(p0: MotionEvent?) {
+            }
+
+            override fun onSingleTapUp(p0: MotionEvent?): Boolean {
+                isRaining = !isRaining
+                val benNode = ARArbiTrack.getInstance().world.findChildByName(ARObjects.BigBen.name)
+                return true
+            }
+
+            override fun onDown(p0: MotionEvent?): Boolean {
+                return true
+            }
+
+            override fun onFling(p0: MotionEvent?, p1: MotionEvent?, p2: Float, p3: Float): Boolean {
+                return true
+            }
+
+            override fun onScroll(p0: MotionEvent?, p1: MotionEvent?, p2: Float, p3: Float): Boolean {
+                val ben = ARArbiTrack.getInstance().world.findChildByName(ARObjects.BigBen.name).apply {
+                    rotateByDegrees(p2-p3, 0f, 1f, 0f)
+                }
+                return true
+
+            }
+
+            override fun onLongPress(p0: MotionEvent?) {
+            }
+        });
+    }
+    private val trackableId = "Tracker"
+    private var isRaining = false
     private val trackable by lazy {
         ARImageTrackable(trackableId).apply {
             loadFromAsset("target.png")
@@ -25,6 +58,10 @@ class MainActivity : ARActivity(), ARArbiTrackListener {
         setContentView(R.layout.activity_main)
     }
 
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        gestureDetector.onTouchEvent(event)
+        return super.onTouchEvent(event)
+    }
     override fun setup() {
         super.setup()
         with(ARImageTracker.getInstance()) {
@@ -34,17 +71,20 @@ class MainActivity : ARActivity(), ARArbiTrackListener {
 
         val bigBen = createBigBen()
         val hearts = listOf(
-            createHearth(1500f, 2500f, 500f),
-            createHearth(-1500f, -2500f, 750f),
+            createHearth(1500f, 9000f, 500f),
+            createHearth(-1500f, 6500f, 750f),
             createHearth(2000f, 1500f, -500f),
-            createHearth(1250f, 1400f, -750f),
+            createHearth(1250f, 3400f, -750f),
             createHearth(-1250f, 350f, 400f),
-            createHearth(-2000f, -3500f, -500f),
-            createHearth(-500f, -5500f, -750f),
-            createHearth(1250f, 350f, 400f),
-            createHearth(1250f, -400f, -400f),
-            createHearth(-1200f, 1250f, 1500f)
+            createHearth(-2000f, 4500f, -500f),
+            createHearth(-500f, 2500f, -750f),
+            createHearth(1250f, 5350f, 400f),
+            createHearth(1250f, 4600f, -400f),
+            createHearth(-1200f, 7250f, 1500f)
         )
+        val cloud = createCloud(0f,10000f,0f);
+
+        bigBen.addChild(cloud)
         hearts.forEach(bigBen::addChild)
 
         trackable.world.addChildren(listOf(bigBen))
@@ -66,10 +106,7 @@ class MainActivity : ARActivity(), ARArbiTrackListener {
             val arbiTrack = ARArbiTrack.getInstance()
 
             val cloud = trackable.world.findChildByName(ARObjects.Cloud.name)
-            val benNode = trackable.world.findChildByName(ARObjects.BigBen.name).apply {
-                setOrientation(1f, 1f, 1f, 1f)
-                rotateByDegrees(-90f, 0f, 0f, 1f)
-            }
+            val benNode = trackable.world.findChildByName(ARObjects.BigBen.name)
 
             val benOrientation = benNode.world.worldOrientation.mult(benNode.worldOrientation)
             benNode.orientation = arbiTrack.world.orientation.inverse().mult(benOrientation)
@@ -80,7 +117,7 @@ class MainActivity : ARActivity(), ARArbiTrackListener {
                 benNode
                     .children
                     .filter { it.isHearth() }
-                    .forEach { it.goUpAnimation() }
+                    .forEach { it.rainAnimation() }
             }
 
             firstRun = false
@@ -110,8 +147,12 @@ class MainActivity : ARActivity(), ARArbiTrackListener {
 
         }
 
+        val texture2D = ARTexture2D()
+        texture2D.loadFromAsset("clouds.png")
+
         val material = ARLightMaterial().apply {
             setAmbient(0.8f, 0.8f, 0.8f)
+            setTexture(texture2D)
         }
 
         // Apply texture material to models mesh nodes
@@ -122,7 +163,7 @@ class MainActivity : ARActivity(), ARArbiTrackListener {
         return modelImporter.node.apply {
             name = ARObjects.Cloud.name
             setPosition(x, y, z)
-            scaleByUniform(10f)
+            scaleByUniform(.5f)
         }
 
     }
@@ -134,6 +175,8 @@ class MainActivity : ARActivity(), ARArbiTrackListener {
         val modelNode = modelImporter.node.apply {
             rotateByDegrees(-90f, 0f, 0f, 0f)
             scaleByUniform(0.1f)
+            setOrientation(1f, 1f, 1f, 1f)
+            rotateByDegrees(-90f, 0f, 0f, 1f)
             name = ARObjects.BigBen.name
         }
 
@@ -159,19 +202,20 @@ class MainActivity : ARActivity(), ARArbiTrackListener {
         name = ARObjects.Hearth.name
         setPosition(x, y, z)
         scaleByUniform(0.4f)
-
+        visible = isRaining
     }
 
     private fun ARNode.isHearth() = name.contentEquals(ARObjects.Hearth.name)
 
-    private fun ARNode.goUpAnimation(incerment: Int = 10) {
+    private fun ARNode.rainAnimation(incerment: Int = 10) {
         val x = position.getX()
         val y = position.getY()
         val z = position.getZ()
-        if (y > 10000)
-            setPosition(x, 0f, z)
-        else
-            setPosition(x, y + incerment, z)
+        if (y < 0){
+            setPosition(x, 10000f, z)
+            visible = isRaining
+        }else
+            setPosition(x, y - incerment, z)
     }
 
 }
